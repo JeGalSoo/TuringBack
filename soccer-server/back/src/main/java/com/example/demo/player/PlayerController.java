93f7f43f21c8fc165122f9460a541b091a61d403
BKWANGHO <bkwangho@naver.com>
2024-04-29 11:17:19 +0900

    create player

---
+++ soccer-server/back/src/main/java/com/example/demo/player/PlayerController.java
@@ -0,0 +1,4 @@
+package com.example.demo.player;
+
+public class PlayerController {
+}
---
+++ soccer-server/back/src/main/java/com/example/demo/player/model/Player.java
@@ -0,0 +1,37 @@
+package com.example.demo.player.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String playerId;
+    String playerName;
+    String ePlayerName;
+    @Column(name = "join_yyyy")
+    String joinYyyy;
+    String position;
+    Long backNo;
+    String nation;
+    Date birthDate;
+    String solar;
+    Long height;
+    Long weiget;
+    String teamId;
+
+
+
+
+
+}
