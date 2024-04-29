527324bde4be4d6d5bd7c0102a94f2639be2f919
JeGalSoo <jgs0318@gmail.com>
2024-04-29 17:42:43 +0900

    240429

--- soccer-server/back/src/main/java/com/turing/api/team/model/Team.java
+++ soccer-server/back/src/main/java/com/turing/api/team/model/Team.java
@@ -40,7 +40,7 @@ public class Team extends BaseEntity {
     private String owner;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "stadium_id", nullable = true, referencedColumnName = "team_id", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
+    @JoinColumn(name = "stadium_id", nullable = true, referencedColumnName = "stadium_id", foreignKey = @ForeignKey(value = ConstraintMode.NO_CONSTRAINT))
     private Stadium stadiumId;
 
     @OneToMany(mappedBy = "teamId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
