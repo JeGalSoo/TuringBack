1ae363802f825e79c36d75550e7462f61b8dbcf1
JeGalSoo <jgs0318@gmail.com>
2024-04-29 15:08:15 +0900

    240429

--- soccer-server/back/src/main/java/com/turing/api/stadium/model/StadiumDto.java
+++ soccer-server/back/src/main/java/com/turing/api/stadium/model/StadiumDto.java
@@ -1,4 +1,12 @@
-package com.example.demo.stadium.model;
+package com.turing.api.stadium.model;
 
 public class StadiumDto {
+    private Long id;
+    private String stadiumId;
+    private String stadiumName;
+    private String hometeamId;
+    private Integer seatCount;
+    private String address;
+    private String ddd;
+    private String tel;
 }
