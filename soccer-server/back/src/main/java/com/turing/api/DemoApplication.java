2b7024d6361929bf403f3abd15ee7110d87d7486
wawaHyun <sjrkchdkdy@gmail.com>
2024-05-03 17:58:42 +0900

    240503

--- soccer-server/back/src/main/java/com/turing/api/DemoApplication.java
+++ soccer-server/back/src/main/java/com/turing/api/DemoApplication.java
@@ -2,7 +2,13 @@ package com.turing.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +16,8 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
     }
+
 
 }
